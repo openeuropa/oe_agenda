17d2d8bcc489bc704bcbc231ec53838092725575
Péter Keszthelyi <keszthelyi.peter@gmail.com>
2023-08-02 09:54:31 +0200

    UCPKN-2281: Add `Day` sub-entity and `Day type` sub-entity type.

---
+++ src/Entity/Day.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_agenda\Entity;
+
+use Drupal\oe_content_sub_entity\Entity\SubEntityBase;
+
+/**
+ * Defines the day entity.
+ *
+ * @ContentEntityType(
+ *   id = "oe_agenda_day",
+ *   label = @Translation("Day"),
+ *   label_collection = @Translation("Days"),
+ *   bundle_label = @Translation("Type"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "access" = "Drupal\oe_content_sub_entity\SubEntityAccessControlHandler",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "translation" = "Drupal\content_translation\ContentTranslationHandler",
+ *     "form" = {
+ *       "default" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "edit" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *   },
+ *   base_table = "oe_agenda_day",
+ *   data_table = "oe_agenda_day_field_data",
+ *   revision_table = "oe_agenda_day_revision",
+ *   revision_data_table = "oe_agenda_day_field_revision",
+ *   translatable = TRUE,
+ *   entity_revision_parent_type_field = "parent_type",
+ *   entity_revision_parent_id_field = "parent_id",
+ *   entity_revision_parent_field_name_field = "parent_field_name",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "revision_id",
+ *     "bundle" = "type",
+ *     "uuid" = "uuid",
+ *     "langcode" = "langcode",
+ *     "published" = "status",
+ *   },
+ *   bundle_entity_type = "oe_agenda_day_type",
+ *   field_ui_base_route = "entity.oe_agenda_day_type.edit_form",
+ *   content_translation_ui_skip = TRUE,
+ * )
+ */
+class Day extends SubEntityBase implements DayInterface {}
---
+++ src/Entity/DayType.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_agenda\Entity;
+
+use Drupal\oe_content_sub_entity\Entity\SubEntityTypeBase;
+
+/**
+ * Defines the agenda day type entity.
+ *
+ * @ConfigEntityType(
+ *   id = "oe_agenda_day_type",
+ *   label = @Translation("Agenda day type"),
+ *   bundle_of = "oe_agenda_day",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   config_prefix = "oe_agenda_day_type",
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description",
+ *   },
+ *   handlers = {
+ *     "access" = "Drupal\oe_content_entity\CorporateEntityTypeAccessControlHandler",
+ *     "list_builder" = "Drupal\oe_content\Entity\EntityTypeListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\oe_content\Form\EntityTypeForm",
+ *       "add" = "Drupal\oe_content\Form\EntityTypeForm",
+ *       "edit" = "Drupal\oe_content\Form\EntityTypeForm",
+ *       "delete" = "Drupal\oe_content\Form\EntityTypeDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   admin_permission = "administer sub entity types",
+ *   links = {
+ *     "add-form" = "/admin/structure/oe_agenda_day_type/add",
+ *     "edit-form" = "/admin/structure/oe_agenda_day_type/{oe_agenda_day_type}/edit",
+ *     "delete-form" = "/admin/structure/oe_agenda_day_type/{oe_agenda_day_type}/delete",
+ *     "collection" = "/admin/structure/oe_agenda_day_type",
+ *   }
+ * )
+ */
+class DayType extends SubEntityTypeBase {}
